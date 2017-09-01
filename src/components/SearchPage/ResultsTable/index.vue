66c1f7bed96bee8e0cdfc60d52cc830deb38c022
Kipris <pral-93@mail.ru>
2017-09-01 22:09:51 +0300

    added mock

--- src/components/SearchPage/ResultsTable/index.vue
+++ src/components/SearchPage/ResultsTable/index.vue
@@ -27,9 +27,6 @@
             <div>
               st.F.audi a6.un97
             </div>
-            <div>
-              st.F.audi a6.un97
-            </div>
           </td>
           <td class="naming">
             <div>Датчик включения стоп-сигнала AUDI A3 , A4 , A6/S6 , Allroad , Q7 , R8 , SEAT Altea , Arosa , Exeo , Leon , Toledo III , SKODA Octavia , Superb , VOLKSWAGEN Caddy III , Fox , Golf IV , Golf V , Golf VI , Jetta , Lupo , Passat , Phaeton , Polo , Spacefox ,</div>
@@ -37,10 +34,12 @@
           <td class="availability">
             <div>
               <label class="chip chip-green">1</label>
+              <label class="chip chip-blue">1</label>
               <label class="chip chip-grey">5</label>
             </div>            
           </td>
           <td class="price">
+            <div>2 750 Р</div>
             <div>2 750 Р</div>
             <div>2 750 Р</div>
           </td>
@@ -53,9 +52,6 @@
             <div>
               st.F.audi a6.un97
             </div>
-            <div>
-              st.F.audi a6.un97
-            </div>
           </td>
           <td class="naming">
             <div>Датчик включения стоп-сигнала AUDI A3 , A4 , A6/S6 , Allroad , Q7 , R8 , SEAT Altea , Arosa , Exeo , Leon , Toledo III , SKODA Octavia , Superb , VOLKSWAGEN Caddy III , Fox , Golf IV , Golf V , Golf VI , Jetta , Lupo , Passat , Phaeton , Polo , Spacefox ,</div>
@@ -63,10 +59,12 @@
           <td class="availability">
             <div>
               <label class="chip chip-green">1</label>
+              <label class="chip chip-blue">1</label>
               <label class="chip chip-grey">5</label>
             </div>            
           </td>
           <td class="price">
+            <div>2 750 Р</div>
             <div>2 750 Р</div>
             <div>2 750 Р</div>
           </td>
@@ -79,9 +77,6 @@
             <div>
               st.F.audi a6.un97
             </div>
-            <div>
-              st.F.audi a6.un97
-            </div>
           </td>
           <td class="naming">
             <div>Датчик включения стоп-сигнала AUDI A3 , A4 , A6/S6 , Allroad , Q7 , R8 , SEAT Altea , Arosa , Exeo , Leon , Toledo III , SKODA Octavia , Superb , VOLKSWAGEN Caddy III , Fox , Golf IV , Golf V , Golf VI , Jetta , Lupo , Passat , Phaeton , Polo , Spacefox ,</div>
@@ -89,36 +84,12 @@
           <td class="availability">
             <div>
               <label class="chip chip-green">1</label>
+              <label class="chip chip-blue">1</label>
               <label class="chip chip-grey">5</label>
             </div>            
           </td>
           <td class="price">
             <div>2 750 Р</div>
-            <div>2 750 Р</div>
-          </td>
-        </tr>
-        <tr>
-          <td class="producer">
-            <div>КМК Glass/Бор</div>
-          </td>
-          <td class="vendor">
-            <div>
-              st.F.audi a6.un97
-            </div>
-            <div>
-              st.F.audi a6.un97
-            </div>
-          </td>
-          <td class="naming">
-            <div>Датчик включения стоп-сигнала AUDI A3 , A4 , A6/S6 , Allroad , Q7 , R8 , SEAT Altea , Arosa , Exeo , Leon , Toledo III , SKODA Octavia , Superb , VOLKSWAGEN Caddy III , Fox , Golf IV , Golf V , Golf VI , Jetta , Lupo , Passat , Phaeton , Polo , Spacefox ,</div>
-          </td>  
-          <td class="availability">
-            <div>
-              <label class="chip chip-green">1</label>
-              <label class="chip chip-grey">5</label>
-            </div>            
-          </td>
-          <td class="price">
             <div>2 750 Р</div>
             <div>2 750 Р</div>
           </td>
