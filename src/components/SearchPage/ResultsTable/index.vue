8fa5ffc7545a6cf743accace608b7835ff61a5ac
Alexander Dubrovin <legys5@yandex.ru>
2017-09-05 21:57:03 +0300

    fix package.json

--- src/components/SearchPage/ResultsTable/index.vue
+++ src/components/SearchPage/ResultsTable/index.vue
@@ -44,56 +44,6 @@
             <div>{{detail.nonAvailablePrice}} Р</div>
           </td>
         </tr>
-        <!-- <tr>
-          <td class="producer">
-            <div>КМК Glass/Бор</div>
-          </td>
-          <td class="vendor">
-            <div>
-              st.F.audi a6.un97
-            </div>
-          </td>
-          <td class="naming">
-            <div>Датчик включения стоп-сигнала AUDI A3 , A4 , A6/S6 , Allroad , Q7 , R8 , SEAT Altea , Arosa , Exeo , Leon , Toledo III , SKODA Octavia , Superb , VOLKSWAGEN Caddy III , Fox , Golf IV , Golf V , Golf VI , Jetta , Lupo , Passat , Phaeton , Polo , Spacefox ,</div>
-          </td>  
-          <td class="availability">
-            <div>
-              <label class="chip chip-green">1</label>
-              <label class="chip chip-blue">1</label>
-              <label class="chip chip-grey">5</label>
-            </div>            
-          </td>
-          <td class="price">
-            <div>2 750 Р</div>
-            <div>2 750 Р</div>
-            <div>2 750 Р</div>
-          </td>
-        </tr>
-        <tr>
-          <td class="producer">
-            <div>КМК Glass/Бор</div>
-          </td>
-          <td class="vendor">
-            <div>
-              st.F.audi a6.un97
-            </div>
-          </td>
-          <td class="naming">
-            <div>Датчик включения стоп-сигнала AUDI A3 , A4 , A6/S6 , Allroad , Q7 , R8 , SEAT Altea , Arosa , Exeo , Leon , Toledo III , SKODA Octavia , Superb , VOLKSWAGEN Caddy III , Fox , Golf IV , Golf V , Golf VI , Jetta , Lupo , Passat , Phaeton , Polo , Spacefox ,</div>
-          </td>  
-          <td class="availability">
-            <div>
-              <label class="chip chip-green">1</label>
-              <label class="chip chip-blue">1</label>
-              <label class="chip chip-grey">5</label>
-            </div>            
-          </td>
-          <td class="price">
-            <div>2 750 Р</div>
-            <div>2 750 Р</div>
-            <div>2 750 Р</div>
-          </td>
-        </tr> -->
       </tbody>
     </table>
   </div>
