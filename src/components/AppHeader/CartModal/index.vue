18892e3e475fab491301d92dc4b8eaf2577a2480
Alexandr Dubrovin <legys5@yandex.ru>
2017-09-12 19:31:10 +0300

    test transition

--- src/components/AppHeader/CartModal/index.vue
+++ src/components/AppHeader/CartModal/index.vue
@@ -41,22 +41,33 @@
 
         <div class="options">
           <div class="h1">ДОПОЛНИТЕЛЬНЫЕ ОПЦИИ</div>
-          <div class="promocode">
-            <span class="promocode-icon"></span>
-            <span class="title">Промокод</span>
-            <span class="chevron-icon"></span>
-          </div>
-          <div class="delivery">
-            <span class="promocode-icon"></span>
-            <span class="title">Доставка</span>
-            <span class="chevron-icon"></span>
-          </div>
-          <div class="payment-variants">
-            <div class="heading">
-              <span class="payment-icon"></span>
-              <span class="title">Варианты оплаты</span>
+          
+            <div class="promocode" 
+                 @click="openPromo = !openPromo">
+              <span class="promocode-icon"></span>
+              <span class="title">Промокод</span>
+              <span class="chevron-icon"></span>
+                <transition name="fade">
+                  <div class="transition-wrap"
+                        v-if="openPromo">
+                        Content
+                  </div>
+                </transition>
+            </div>
+          <transition name="fade">
+            <div class="delivery">
+              <span class="promocode-icon"></span>
+              <span class="title">Доставка</span>
               <span class="chevron-icon"></span>
             </div>
+          </transition> 
+          <transition name="fade">
+            <div class="payment-variants">
+              <div class="heading">
+                <span class="payment-icon"></span>
+                <span class="title">Варианты оплаты</span>
+                <span class="chevron-icon"></span>
+              </div>
             <div class="body">
               <div>Оплата электронными способами оплаты с помощью Robokassa 
                 (Электронным кошельком, Через интернет-банк, Банковской картой, 
@@ -76,7 +87,7 @@
             </div>
             
           </div>
-
+          </transition>
 
 
         </div>
@@ -107,10 +118,22 @@
 <script>
 
 export default {
-  name: 'CartModal'
+  name: 'CartModal',
+  data() {
+    return {
+      openPromo: false
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   @import 'styles';
+  
+  .fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0
+}
 </style>
